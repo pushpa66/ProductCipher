19704394f120a4df195f6e541689648aa1879f87
pushpe <pushpakumara.14@cse.mrt.ac.lk>
2017-04-05 00:20:14 +0530

    I achieve more confusion and defusion

--- src/productcipher/ProductCipher.java
+++ src/productcipher/ProductCipher.java
@@ -10,7 +10,8 @@ public class ProductCipher {
     private static BufferedReader br = null;
     private static FileReader fr = null;
     private static String FILENAME;
-    private static int blockLength = 5;
+    private static int blockLength = 1;
+    private static int numberOfRepeatCycles = 1;
     private static String plainText = "";
     
     public static void main(String[] args) {
@@ -28,10 +29,12 @@ public class ProductCipher {
 
             br = new BufferedReader(new FileReader(FILENAME));
             ProductCipher.blockLength = Integer.parseInt(br.readLine());
+            ProductCipher.numberOfRepeatCycles = Integer.parseInt(br.readLine());
 
+             ProductCipher.plainText = br.readLine();
             while ((currentLine = br.readLine()) != null) {
                 
-                ProductCipher.plainText += currentLine;
+                ProductCipher.plainText += " "+currentLine;
             }
             System.out.println("This is the block length specified: "+ ProductCipher.blockLength);
             System.out.println("This is the Plain Text input:");
@@ -42,61 +45,69 @@ public class ProductCipher {
         
         String substitutionInput = ProductCipher.plainText;
         int n = ProductCipher.blockLength;
+        int x = ProductCipher.numberOfRepeatCycles;
 
-        // Substitution encryption
-        StringBuffer substitutionOutput = new StringBuffer();
-        for(int i=0 ; i<substitutionInput.length() ; i++) {
-            char c = substitutionInput.charAt(i);
-            substitutionOutput.append((char) (c+5));
-        }
-        System.out.println("\nSubstituted text:");
-        System.out.println(substitutionOutput);
-
-        // Transposition encryption
-        String transpositionInput = substitutionOutput.toString();
-        int modulus;
-        if((modulus = transpositionInput.length()%n) != 0) {
-            modulus = n-modulus;
-            // ‘modulus’ is now the number of blanks/padding (X) to be appended
-
-            while(modulus != 0){
-                transpositionInput += "/";
-                modulus--;
-            }
-        }
         StringBuffer transpositionOutput = new StringBuffer();
-        System.out.println("\nTransposition Matrix:");
-        for(int i=0 ; i<n ; i++) {
-            for(int j=0 ; j<transpositionInput.length()/n ; j++) {
-                char c = transpositionInput.charAt(i+(j*n));
-                System.out.print(c);
-                transpositionOutput.append(c);
+        
+        for (int repeat = 1; repeat <= x; repeat++) {
+            // Substitution encryption
+            StringBuffer substitutionOutput = new StringBuffer();
+            for(int i=0 ; i<substitutionInput.length() ; i++) {
+                char c = substitutionInput.charAt(i);
+                substitutionOutput.append((char) (c+5));
             }
-            System.out.println();
-        }
-        System.out.println("\nFinal encrypted text:");
-        System.out.println(transpositionOutput);
+            System.out.println("\nSubstituted text at the "+repeat+" cycle::: "+substitutionOutput);
 
-        // Transposition decryption
-        n = transpositionOutput.length()/n;
-        StringBuffer transpositionPlaintext = new StringBuffer();
-        for(int i=0 ; i<n ; i++) {
-            for(int j=0 ; j<transpositionOutput.length()/n ; j++) {
-                char c = transpositionOutput.charAt(i+(j*n));
-                transpositionPlaintext.append(c);
+            // Transposition encryption
+            String transpositionInput = substitutionOutput.toString();
+            int modulus;
+            if((modulus = transpositionInput.length()%n) != 0) {
+                modulus = n-modulus;
+                // plain text input length must be divisible by blockLength. Therefore we have to add required extra chars
+                while(modulus != 0){
+                    transpositionInput += "/";
+                    modulus--;
+                }
             }
+            StringBuffer tempTranspositionOutput = new StringBuffer();
+            System.out.println("\nTransposition Matrix at the "+repeat+" cycle:::");
+            for(int i=0 ; i<n ; i++) {
+                for(int j=0 ; j<transpositionInput.length()/n ; j++) {
+                    char c = transpositionInput.charAt(i+(j*n));
+                    System.out.print(c);
+                    tempTranspositionOutput.append(c);
+                }
+                System.out.println();
+            }
+            System.out.println("\nFinal encrypted text at the end of "+repeat+" cycle::: "+tempTranspositionOutput);
+            
+            substitutionInput = tempTranspositionOutput.toString();
+            transpositionOutput = tempTranspositionOutput;
         }
+        
+        for (int repeat = x; repeat > 0; repeat--) {
+            // Transposition decryption
+            int temp = n;
+            n = transpositionOutput.length()/n;
+            StringBuffer transpositionPlaintext = new StringBuffer();
+            for(int i=0 ; i<n ; i++) {
+                for(int j=0 ; j<transpositionOutput.length()/n ; j++) {
+                    char c = transpositionOutput.charAt(i+(j*n));
+                    transpositionPlaintext.append(c);
+                }
+            }
 
-        // Substitution decryption
-        StringBuffer plaintext = new StringBuffer();
-        for(int i=0 ; i<transpositionPlaintext.length() ; i++) {
-            char c = transpositionPlaintext.charAt(i);
-            plaintext.append((char) (c-5));
-        }
+            // Substitution decryption
+            StringBuffer plaintext = new StringBuffer();
+            for(int i=0 ; i<transpositionPlaintext.length() ; i++) {
+                char c = transpositionPlaintext.charAt(i);
+                plaintext.append((char) (c-5));
+            }
 
-        System.out.println("\nPlaintext:");
-        System.out.println(plaintext);
-         
+            System.out.println("\nPlaintext at the end of "+(x - repeat+1)+ " decryption cycle::: "+plaintext);
+            
+            n = temp;
+            transpositionOutput = plaintext;
+        }  
     }
-    
 }
